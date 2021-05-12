0023048c6a3eec8ea099cdaaa0be85d78e912896
Empteyy <57791584+Empteyy@users.noreply.github.com>
2021-05-12 09:55:49 +0200

    Added project

---
+++ Assets/Scripts/GridManager.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridManager : MonoBehaviour
+{
+    public float[,] Grid;
+    int Vertical, Horizontal, Colums, Rows;
+    public Camera mainCamera;
+    public Sprite sprite;
+    public GameObject player;
+    
+
+    void Start()
+    {
+        Vertical = (int)mainCamera.orthographicSize;
+        Horizontal = Vertical * (Screen.width / Screen.height);
+        Colums = Horizontal * 2;
+        Rows = Vertical * 2;
+        Grid = new float[Colums, Rows];
+        for (int i = 0; i < Colums; i++)
+        {
+            for (int j = 0; j < Rows; j++)
+            {
+                Grid[i, j] = Random.Range(0f, 1f);
+                SpawnTile(i, j, Grid[i, j]);
+                
+            }
+        }
+
+        SpawnPlayer();
+    }
+
+    void Update()
+    {
+        
+    }
+
+    void SpawnTile(int x, int y, float value)
+    {
+        GameObject g = new GameObject(x + " : " + y);
+        g.transform.position = new Vector3(x - (Horizontal - 0.5f), y - (Vertical - 0.5f));
+        var s = g.AddComponent<SpriteRenderer>();
+        s.sprite = sprite;
+    }
+
+    void SpawnPlayer()
+    {
+        player.transform.position = new Vector3(0.5f, 0.5f, 0.5f);
+        Instantiate(player);
+    }
+
+    
+}
---
+++ Assets/Scripts/TileMovement.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileMovement : MonoBehaviour
+{
+    float speed = 5;
+    public Vector3 currentPos;
+    Vector3 targetPos;
+    bool moving;
+
+    void Update()
+    {
+        MovePlayer();
+    }
+
+    void MovePlayer()
+    {
+        if (moving)
+        {
+            if (Vector3.Distance(currentPos, transform.position) > 1f)
+            {
+                transform.position = targetPos;
+                moving = false;
+                return;
+            }
+
+            transform.position += (targetPos - currentPos) * Time.deltaTime;
+            return;
+        }
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            targetPos = transform.position + Vector3.up;
+            currentPos = transform.position;
+            moving = true;
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            targetPos = transform.position + Vector3.down;
+            currentPos = transform.position;
+            moving = true;
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            targetPos = transform.position + Vector3.left;
+            currentPos = transform.position;
+            moving = true;
+            transform.localRotation = Quaternion.Euler(0, 180, 0);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            targetPos = transform.position + Vector3.right;
+            currentPos = transform.position;
+            moving = true;
+            transform.localRotation = Quaternion.Euler(0, 0, 0);
+        }
+    }
+}
